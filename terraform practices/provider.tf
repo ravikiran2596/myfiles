d38ef976342e6cc230bb71c736f2c62fc8e32933
ravikiran2596 <ravikiranmudiraj.rkm@gmail.com>
2023-04-05 11:54:55 +0530

    added

--- terraform practices/provider.tf
+++ terraform practices/provider.tf
@@ -1,3 +1,13 @@
-resource "" "name" {
+terraform {
+  required_version = ">=1.4.0"
+  required_providers {
+    shell = {
+      source = "scottwinkler/shell"
+      version = "1.7.10"
+    }
+  }
+}
+
+provider "shell" {
   
 }
